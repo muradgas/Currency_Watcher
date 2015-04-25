1223c8cd0a297c86e7cecf962d208560d1936667
S-LifeLover <l.rus-lifelover@mail.ru>
2015-04-25 11:00:40 +0300

    Вместо консольного приложения теперь WP-приложение

---
+++ Currency_Watcher/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Currency_Watcher.Resources;
+
+namespace Currency_Watcher
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
